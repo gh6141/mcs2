1521e0483fcbc31b2f11bd6d0843d32443d92216
gh <gh6141@gmail.com>
2022-07-17 12:02:12 +0900

    age range ok

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,6 +7,8 @@ from django.contrib.auth.forms import(
   UserChangeForm as BaseUserChangeFrom,
 )
 
+import datetime
+
 
 class UserCreationForm(BaseUserCreationForm):
   class Meta(BaseUserCreationForm.Meta):
@@ -18,12 +20,18 @@ class UserChangeForm(BaseUserChangeFrom):
 
 
 class ProfileForm(forms.ModelForm):
+
     class Meta:
+        currentDateTime = datetime.datetime.now()
+        date = currentDateTime.date()
+        year = date.strftime("%Y")
         model = User
         fields = ('nickname','sex','date_of_birth','shussin','kyojuchi','shokugyo','shumi','self_introduction','doi_flg','profile_photo1','profile_photo2',
           'profile_photo3','profile_photo4','profile_photo5','tabako','osake','kekkonreki','kodomo',
           'is_admin','is_staff','date_joined','is_active')
-
+        widgets = {
+            'date_of_birth': forms.SelectDateWidget(years=[x for x in range(int(year)-80, int(year)-19)])
+        }
 """
       
     username=
